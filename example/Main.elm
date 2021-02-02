8a33f31590e31268c1bc06a0303962572fddbaf0
Emiliano Bovetti <emiliano.bovetti@prima.it>
2021-02-02 11:43:39 +0100

    Refactor of example to include library changes

--- example/Main.elm
+++ example/Main.elm
@@ -1,12 +1,12 @@
 module Main exposing (..)
 
+import Browser
 import GraphQL.Client.Http as GraphQLClient
 import GraphQL.Request.Builder exposing (..)
 import GraphQL.Request.Builder.Arg as Arg
 import GraphQL.Request.Builder.Variable as Var
-import Html exposing (Html, div, text)
-import Task exposing (Task)
-import Browser
+import Html exposing (Html)
+
 
 {-| Responses to `starWarsRequest` are decoded into this type.
 -}
@@ -30,7 +30,6 @@ fragment filmPlanetsFragment on Film {
     }
   }
 }
-
 query ($filmID: ID!, $pageSize: Int = 3) {
   film(filmID: $filmID) {
     title
@@ -106,27 +105,32 @@ connectionNodes spec =
         )
 
 
-type alias StarWarsResponse =
-    Result GraphQLClient.Error FilmSummary
-
-
 type alias Model =
-    Maybe StarWarsResponse
+    Maybe FilmSummary
 
 
 type Msg
-    = ReceiveQueryResponse StarWarsResponse
+    = ReceiveQueryResponse FilmSummary
+    | ReceiveQueryError
 
 
-sendQueryRequest : Request Query a -> Task GraphQLClient.Error a
+graphQLToMsg : GraphQLClient.Result FilmSummary -> Msg
+graphQLToMsg result =
+    case result of
+        GraphQLClient.GraphQLSucces data ->
+            ReceiveQueryResponse data
+
+        -- Explicitly ignoring GraphQL data
+        GraphQLClient.GraphQLErrors _ _ ->
+            ReceiveQueryError
+
+        GraphQLClient.HttpError _ ->
+            ReceiveQueryError
+
+
+sendQueryRequest : Request Query FilmSummary -> Cmd Msg
 sendQueryRequest request =
-    GraphQLClient.sendQuery "/" request
-
-
-sendStarWarsQuery : Cmd Msg
-sendStarWarsQuery =
-    sendQueryRequest starWarsRequest
-        |> Task.attempt ReceiveQueryResponse
+    GraphQLClient.sendQuery "/" graphQLToMsg request
 
 
 main : Program () Model Msg
@@ -141,16 +145,55 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( Nothing, sendStarWarsQuery )
+    ( Nothing, sendQueryRequest starWarsRequest )
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Example"
-    , body = [ model |> Debug.toString |> text ]
+    , body =
+        [ Maybe.map viewFilmSummary model |> Maybe.withDefault (Html.text "Nothing") ]
     }
 
 
+viewFilmSummary : FilmSummary -> Html Msg
+viewFilmSummary summary =
+    Html.div []
+        [ Html.text ("Title: " ++ Maybe.withDefault "Unknown" summary.title)
+        , viewCharacterNames summary.someCharacterNames
+        , viewPlanetNames <| Maybe.withDefault [] summary.somePlanetNames
+        ]
+
+
+viewCharacterNames : List (Maybe String) -> Html Msg
+viewCharacterNames names =
+    Html.div []
+        [ Html.text "Character names: "
+        , viewNameList names
+        ]
+
+
+viewPlanetNames : List (Maybe String) -> Html Msg
+viewPlanetNames names =
+    Html.div []
+        [ Html.text "Planet names: "
+        , viewNameList names
+        ]
+
+
+viewNameList : List (Maybe String) -> Html Msg
+viewNameList names =
+    names
+        |> List.map (Maybe.withDefault " -- ")
+        |> String.join ", "
+        |> Html.text
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
-update (ReceiveQueryResponse response) model =
-    ( Just response, Cmd.none )
+update msg model =
+    case msg of
+        ReceiveQueryResponse data ->
+            ( Just data, Cmd.none )
+
+        ReceiveQueryError ->
+            ( Nothing, Cmd.none )
